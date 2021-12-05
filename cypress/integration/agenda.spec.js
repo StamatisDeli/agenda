49b8dcfd67fe45e73c00c38f0e877076e89a49cf
Stamatis Deliyannis <sdeliyannis@helloworld.gr>
2021-12-06 00:15:20 +0200

    feat: Add cypress

--- cypress/integration/agenda.spec.js
+++ cypress/integration/agenda.spec.js
@@ -34,16 +34,6 @@ describe("Agenda", () => {
     );
   });
 
-  const r = {
-    id: "5c093af1c6ee9117a581c7d6",
-    photo: "https://randomuser.me/api/portraits/men/40.jpg",
-    name: "Master Yoda",
-    company: "ZOLAREX",
-    email: "bates.washington@zolarex.io",
-    phone: "+1 (915) 447-2207",
-    address: "958 Brevoort Place, Ona, Maine, 2433",
-  };
-
   it("renders the data of the selected user on the right section within a form", function () {
     cy.get("[data-testid=5c093af1c6ee9117a581c7d6]").click();
 
